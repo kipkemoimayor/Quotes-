0a5a69a6a2257d5069bb617f5b5059475dc84093
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-23 10:33:00 +0300

    Added a function bgHighest

--- src/app/highest.directive.ts
+++ src/app/highest.directive.ts
@@ -4,11 +4,16 @@ import { Directive,ElementRef,HostListener} from '@angular/core';
   selector: '[appHighest]'
 })
 export class HighestDirective {
+  @HostListener('mouseenter') onMouseEnter(){
+    this.bgHighest("#1885");
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.bgHighest(null)
+  }
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.backgroundColor="green";
-
-
+  constructor(private elem:ElementRef) {}
+  private bgHighest(color:string){
+    this.elem.nativeElement.style.backgroundColor=color;
 
   }
 
